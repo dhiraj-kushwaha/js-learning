40448ba17fbff67fab85720cd24119214d9ba414
Dhiraj Kumar Kushwaha <74665257+dhiraj-kushwaha@users.noreply.github.com>
2024-03-09 10:33:16 +0000

    09march

--- basic_01/06-1_string.js
+++ basic_01/06-1_string.js
@@ -1,6 +1,38 @@
 const name1 ='dhiraj';
 const letterCount = 6;
 
-console.log(name1+letterCount);
+// console.log(name1+letterCount);
 
-console.log(`hello dhiraj kushwaha ${name1} and number of letters are ${letterCount}` )
+// console.log(`hello dhiraj kushwaha ${name1} and number of letters are ${letterCount}` )
+
+//prototype methods in string
+
+
+ const name2 = new String ('dhirajkushwaha')
+// console.log(name2.length)
+// console.log(name2.toUpperCase())
+// console.log(name2.indexOf('h'));
+
+const newstring = name2.substring(0,5)
+// console.log(newstring)
+
+const newstring2 = name2.slice(-11,6);
+// console.log(newstring2)
+
+//------------trim----------
+
+const trimSpace="            dhiraj jkajij hhs"
+const newString3 = trimSpace.trim()
+console.log(newString3)
+
+const trimU = "   diraj  jsdf  "
+console.log(trimU.trim())
+
+const url ="www.google.com"
+// console.log(url.replace('google','pronhub'))
+// console.log(url.includes('google'))
+
+//split use to sparate 
+
+const splitTry = "dhiraj*dj-jd"
+console.log(splitTry.split('*'));
--- js_basic_02/09_array.js
+++ js_basic_02/09_array.js
@@ -1,42 +1,93 @@
-// // array
+// // // array
 
- const myArr =[0,1,2,3,4,5] // string can also be stored in array, can be mix of variable
+//  const myArr =[0,1,2,3,4,5] // string can also be stored in array, can be mix of variable
+//  //made changes in arr changed in the mail file like heap 
 
-// console.log(myArr[1]); //when
+// // console.log(myArr[1]); //when
 
-// const myHeors = ["shaktiman"]
-// const myArr2 = new Array(1,23,56,5,44)
+// // const myHeors = ["shaktiman"]
+// // const myArr2 = new Array(1,23,56,5,44)
 
-// // methods
+// // // methods
 
-// // myArr2.push(77)
-// // myArr2.push(69)
-// // myArr2.pop()
+// // // myArr2.push(77)
+// // // myArr2.push(69)
+// // // myArr2.pop()
 
-// // myArr2.unshift(9)
-// // myArr2.shift()
+// // // myArr2.unshift(9)
+// // // myArr2.shift()
 
+// // console.log(myArr.includes(9))
+// // console.log(myArr2.indexOf(23))
+
+
+// // console.log(myArr2);
+
+// // const newArr = myArr.join()
+
+// // console.log(myArr);
+// // console.log(typeof newArr);
+
+
+// //slice, splice
+
+// console.log("A", myArr)
+
+// const myn1 = myArr.slice(1,3)
+// console.log(myn1);
+// console.log("B", myArr)
+
+// // splice mannupalate the original array but slice dont 
+// const myn2 = myArr.splice(1,3)
+// console.log("c", myArr)
+// console.log(myn2);
+
+
+
+
+
+// 09--3--2-34
+const myArr = new Array(1,2,3,4)
+const myArr1 = new Array(5,6,7,8)
+console.log(myArr[0]);
+// myArr.push(6)
+// myArr.push(8)
+
+// myArr.pop()// last value of arr will be removed
+
+myArr.unshift(9)//add the value in starting
+// myArr.shift()//remove that starting value'
+
+// console.log(myArr)
 // console.log(myArr.includes(9))
-// console.log(myArr2.indexOf(23))
+// console.log(myArr.indexOf(9))
 
+// const newArr = myArr.join()//join also chnage the tyoe of arr from object to string
+// console.log(typeof(myArr))
+// console.log(typeof(newArr))
 
-// console.log(myArr2);
+//slice ,splice
+console.log("A",myArr)
 
-// const newArr = myArr.join()
-
-// console.log(myArr);
-// console.log(typeof newArr);
-
-
-//slice, splice
-
-console.log("A", myArr)
-
-const myn1 = myArr.slice(1,3)
+const myn1 = myArr.slice(1,3) // it will pick the value starting for the starting index(1), and will go upto (n-1) to the end value.
 console.log(myn1);
-console.log("B", myArr)
+console.log("B",myArr) 
 
-// splice mannupalate the original array but slice dont 
-const myn2 = myArr.splice(1,3)
-console.log("c", myArr)
+//difference between slice and splice is that slice works on n-1(n= end value)but splice works on the end value and splice also change the main arr.
+
+const myn2 = myArr.splice(1,3)// it will pick the vlaue starting form the starting index(1) and will go to the end of the given value.
 console.log(myn2);
+console.log("C",myArr)
+
+
+
+
+
+
+
+
+
+
+
+
+
