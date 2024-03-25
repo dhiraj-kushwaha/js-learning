73d497a91688fce0e533cb5c5adce9bd4592b0a3
Dhiraj Kumar Kushwaha <74665257+dhiraj-kushwaha@users.noreply.github.com>
2024-03-25 15:09:09 +0000

    object

--- js_basic_02/12_objects2.js
+++ js_basic_02/12_objects2.js
@@ -25,5 +25,38 @@ const obj1 = {1:"a", 2: "jj"}
 const obj2 = {3:"as", 4: "jdj"}
 // to marge to object 
 
-const obj3 = {obj1, obj2}
-console.log(obj3);
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign (obj1,obj2) // it will marge the given object in the (==) but we can do it in another way as well
+
+const obj3 = Object.assign ({},obj1,obj2)// {} it works as target,it is not compalsory but it is good to user
+
+const obj4 = {...obj1,...obj2} // it will also marge the object but it will spread the object and then marge it
+console.log(obj4);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },{
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    }
+
+]
+
+users[1].email
+
+console.log(tinderUser)
+console.log(Object.keys(tinderUser))// use to get the alll key in a object and it datatype in array
+
+console.log(Object.entries(tinderUser))
+console.log(tinderUser.hasOwnProperty(isloggIn))
