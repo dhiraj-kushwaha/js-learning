052984da1dfec82fec26af21cc3497f1e890b6dc
Dhiraj Kumar Kushwaha <74665257+dhiraj-kushwaha@users.noreply.github.com>
2024-09-11 03:24:06 +0000

    h

---
+++ basic_03/14.0_function.js
@@ -0,0 +1,17 @@
+function calculateCartPrice(...num1){ // (...) it is a rest opertor,which will help you to bundle the whole
+                                //if (val1,val2,...num1 so it will put the 200 in val1,300 in val2 and remaining in ...num1)
+    return num1
+
+}
+
+//console.log(calculateCartPrice(200,300,400))
+
+const user={
+    username: "dhiraj",
+    price:55
+
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${} and price is $`)
+}
